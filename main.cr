5c35a18de434a6d068795642d106d2062620fe7d
Vasyl Kuzmyk <vasyl.kuzmyk@toptal.com>
2021-09-04 21:54:48 +0300

    Refactor shared places

--- main.cr
+++ main.cr
@@ -3,6 +3,7 @@ require "http/client"
 require "json"
 
 SERVER_URL="https://yelst-backend.herokuapp.com"
+CMD = "sh"
 
 OptionParser.parse do |parser|
   parser.on "-v", "--version", "Show version" do
@@ -26,14 +27,7 @@ OptionParser.parse do |parser|
 
     response = HTTP::Client.post "#{SERVER_URL}/users/sign_up", headers: nil, form: {email: email, password: password}.to_json
 
-    cmd = "sh"
-    args = [] of String
-    token = "echo " + JSON.parse(response.body)["result"].to_s + " > ~/.yelts_token"
-    args << "-c" << token
-
-    puts args
-
-    Process.run(cmd, args, shell: true)
+    write_to_token_file(JSON.parse(response.body)["result"].to_s)
     exit
   end
 
@@ -48,30 +42,21 @@ OptionParser.parse do |parser|
 
     response = HTTP::Client.post "#{SERVER_URL}/users/sign_in", headers: nil, form: {email: email, password: password}.to_json
 
-    cmd = "sh"
-    args = [] of String
-    token = "echo " + JSON.parse(response.body)["result"].to_s + " > ~/.yelts_token"
-    args << "-c" << token
-
-    puts args
-
-    Process.run(cmd, args, shell: true)
+    write_to_token_file(JSON.parse(response.body)["result"].to_s)
     exit
   end
 
   parser.on "scan", "Scan packages" do
-    cmd = "sh"
-
     args = [] of String
     args << "-c" << "pacman -Qq"
     io = IO::Memory.new
-    Process.run(cmd, args, shell: true, output: io)
+    Process.run(CMD, args, shell: true, output: io)
     list = io.to_s.split("\n")
 
     args = [] of String
     args << "-c" << "cat ~/.yelts_token"
     io = IO::Memory.new
-    Process.run(cmd, args, shell: true, output: io)
+    Process.run(CMD, args, shell: true, output: io)
     token = io.to_s.sub("\n", "")
 
     headers =  HTTP::Headers.new.add("Authorization", value: "Bearer #{token}")
@@ -82,42 +67,40 @@ OptionParser.parse do |parser|
   end
 
   parser.on "list", "List of saved packages" do
-    cmd = "sh"
-    args = [] of String
-    args << "-c" << "cat ~/.yelts_token"
-    io = IO::Memory.new
-    Process.run(cmd, args, shell: true, output: io)
-    token = io.to_s.sub("\n", "")
-
-    headers =  HTTP::Headers.new.add("Authorization", value: "Bearer #{token}")
-    response = HTTP::Client.get "#{SERVER_URL}/packages/get_list", headers: headers
-    packages = JSON.parse(response.body)["result"].to_s.split(" ")
-
     puts packages
     exit
   end
 
   parser.on "restore", "Restore packages from list" do
-    cmd = "sh"
-    args = [] of String
-    args << "-c" << "cat ~/.yelts_token"
-    io = IO::Memory.new
-    Process.run(cmd, args, shell: true, output: io)
-    token = io.to_s.sub("\n", "")
-
-    headers =  HTTP::Headers.new.add("Authorization", value: "Bearer #{token}")
-    response = HTTP::Client.get "#{SERVER_URL}/packages/get_list", headers: headers
-    packages = JSON.parse(response.body)["result"].to_s.split(" ")
-
     io = IO::Memory.new
     args = [] of String
     pacman_string = "yay -S "
-    packages = packages.join(" ")
-    pacman_string += packages
+    list_of_packages = packages.join(" ")
+    pacman_string += list_of_packages
     pacman_string += " --noconfirm --needed"
     args << "-c" << pacman_string
-    Process.run(cmd, args, shell: true, output: io)
+    Process.run(CMD, args, shell: true, output: io)
     puts io.to_s
     exit
   end
+end
+
+def packages
+  args = [] of String
+  args << "-c" << "cat ~/.yelts_token"
+  io = IO::Memory.new
+  Process.run(CMD, args, shell: true, output: io)
+  token = io.to_s.sub("\n", "")
+
+  headers =  HTTP::Headers.new.add("Authorization", value: "Bearer #{token}")
+  response = HTTP::Client.get "#{SERVER_URL}/packages/get_list", headers: headers
+  JSON.parse(response.body)["result"].to_s.split(" ")
+end
+
+def write_to_token_file(text)
+  cmd = "sh"
+  args = [] of String
+  token = "echo " + text + " > ~/.yelts_token"
+  args << "-c" << token
+  Process.run(CMD, args, shell: true)
 end
